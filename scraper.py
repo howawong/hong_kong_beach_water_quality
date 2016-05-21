5def0b82042917b6093cdd427cfc7b03b0105f49
Ho Wa Wong <howa.wong@gmail.com>
2016-05-21 01:19:40 +0000

    Initial commit

--- scraper.py
+++ scraper.py
@@ -22,3 +22,33 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+import scraperwiki
+import lxml.html
+import lxml.etree
+import re
+import urllib
+import datetime
+import dateutil.parser
+import re
+def parse_item(item):
+    title = item.xpath("./title")[0].text
+    link = item.xpath("./link")[0].text
+    pub_date = item.xpath("./pubDate")[0].text
+    place, quality = title.split(" was rated as ")
+    place = place.strip()
+    quality = quality.strip()
+    m = re.match('([^\)]*)\((.*)\)', quality)
+    grade_detail =  m.group(2)
+    grade = m.group(1)
+    date_str = dateutil.parser.parse(pub_date).strftime('%Y-%m-%d')
+    scraperwiki.sqlite.save(unique_keys=["place", "date"], data={"place": place, "grade": grade, "grade_detail": grade_detail, "date": date_str})
+    print place, grade, grade_detail, date_str
+
+def parse_rss(url):
+    html = scraperwiki.scrape(url)
+    parser = lxml.etree.XMLParser(strip_cdata=False)
+    root = lxml.etree.fromstring(html, parser)
+    for item in root.xpath('//rss/channel/item'):
+        parse_item(item)
+
+parse_rss("http://beachwq.gov.hk/en/rss2.aspx")
